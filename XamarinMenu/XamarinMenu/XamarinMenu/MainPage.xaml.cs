8693c03e0d2b382b422971a98d121d893a97c171
Steveen Blanco <steveen.blanco@hotmail.com>
2018-01-11 21:42:31 -0600

    Primer check in

---
+++ XamarinMenu/XamarinMenu/XamarinMenu/MainPage.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using XamarinMenu.MenuItems;
+using XamarinMenu.Views;
+
+namespace XamarinMenu
+{
+    public partial class MainPage : MasterDetailPage
+    {
+        public List<MasterPageItem> MenuList { get; set; }
+
+        public MainPage()
+        {
+            InitializeComponent();
+            MenuList = new List<MasterPageItem>();
+
+
+            // Creating our pages for menu navigation
+            // Here you can define title for item, 
+            // icon on the left side, and page that you want to open after selection
+            var page1 = new MasterPageItem() { Title = "Autenticarse", Icon = "itemIcon1.png", TargetType = typeof(Autenticarse) };
+            var page2 = new MasterPageItem() { Title = "Acerca de", Icon = "itemIcon2.png", TargetType = typeof(AcerdaDe) };
+            var page3 = new MasterPageItem() { Title = "Contacto", Icon = "itemIcon3.png", TargetType = typeof(Contacto) };
+            var page4 = new MasterPageItem() { Title = "Salir", Icon = "itemIcon4.png", TargetType = typeof(Salir) };
+
+            // Adding menu items to menuList
+            MenuList.Add(page1);
+            MenuList.Add(page2);
+            MenuList.Add(page3);
+            MenuList.Add(page4);
+
+
+            navigationDrawerList.ItemsSource = MenuList;
+
+            Detail = new NavigationPage((Page)Activator.CreateInstance(typeof(Autenticarse)));
+
+        }
+        private void OnMenuItemSelected(object sender, SelectedItemChangedEventArgs e)
+        {
+
+            var item = (MasterPageItem)e.SelectedItem;
+            Type page = item.TargetType;
+
+            Detail = new NavigationPage((Page)Activator.CreateInstance(page));
+            IsPresented = false;
+        }
+    }
+}
---
+++ XamarinMenu/XamarinMenu/XamarinMenu/ViewModels/Views/Autenticarse.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using XamarinMenu.Models;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace XamarinMenu.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Autenticarse : ContentPage
+    {
+        public Autenticarse()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushModalAsync(new ProveedoresPage());
+
+        }
+      
+    }
+}
